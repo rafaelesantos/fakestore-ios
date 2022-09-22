465782e702090f0e6c2405af935cb9bc834b89f1
Rafael Escaleira <rafaelescaleira@icloud.com>
2022-09-22 01:52:43 -0400

    Create xcode project in swiftui

---
+++ FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by Rafael Escaleira Ferreira dos Santos on 22/09/22.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
